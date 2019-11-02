19d4090e76ac2fe3897194a8aaf1df081c8147f2
filipeFLemos <filipesflemos@gmail.com>
2019-11-02 00:41:58 +0000

    terminal validates transaction and the screen color changes accordingly

---
+++ Project1/TerminalApp/app/src/main/java/com/example/terminalapp/Constants.java
@@ -0,0 +1,16 @@
+package com.example.terminalapp;
+
+public class Constants {
+    static final String TAG = "Repository";
+
+    static final String ACME_REPOSITORY_URL = "http://192.168.1.19:3000/"; //TODO: Change this to your ip
+    static final String TRANSACTIONS = "transactions";
+
+    static final String POST = "POST";
+
+    static final String QR_READER_PLAYSTORE = "market://search?q=pname:com.google.zxing.client.android";
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+
+    static final int KEY_SIZE = 512;
+    static final String ISO_SET = "ISO-8859-1";
+}
