be1ec40d57b34eb7bf0172b02e4eb457cf7c0672
filipeFLemos <filipesflemos@gmail.com>
2019-12-03 17:16:56 +0000

    hourly temperature update

--- Project2/WeatherApp/WeatherApp/WeatherApp/Views/CityTomorrow.xaml.cs
+++ Project2/WeatherApp/WeatherApp/WeatherApp/Views/CityTomorrow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using WeatherApp.Models;
 using Xamarin.Forms;
@@ -9,12 +10,29 @@ namespace WeatherApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CityTomorrow : ContentPage
     {
+        private List<WeatherByHour> WeatherByHours { get; set; }
         public CityTomorrow(City city, DateTime tomorrow)
         {
             InitializeComponent();
 
             CityName.Text = city.Name;
             Date.Text = $"{tomorrow:dddd, MMMM d, yyyy}";
+            
+            WeatherByHours = city.WeatherList.WeatherByDays[1];
+            
+            Temperature.Text = WeatherByHours[0].Temp.ToString(CultureInfo.InvariantCulture);
+
+            var i = 0;
+            foreach (var weather in WeatherByHours)
+            {
+                var name = "Temp" + i;
+                if (this.FindByName(name) is Label tempLabel)
+                {
+                    tempLabel.Text = weather.Temp.ToString(CultureInfo.InvariantCulture) + "°C";
+                }
+
+                i++;
+            }
         }
         
         async void OnFollowBtnClicked(object sender, EventArgs args)
